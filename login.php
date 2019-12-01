a6eab4e0f9bdfde25710c5c07bdf83cfd9c480f1
Destiny Zinke <dzinke@student.neumont.edu>
2019-12-01 10:54:15 -0700

    Initial Commit

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php 
+//login form
+echo '<form>
+<input type="text" placeholder="Username">
+<br/>
+<input type="password" placeholder="Password">
+<br/>
+<input type="submit" value="Submit">
+</form>';
+?>
